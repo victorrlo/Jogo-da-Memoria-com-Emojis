7b11192d4e37c5b4d577c6e3860c50c3dc88e727
victorrlo <42052096+victorrlo@users.noreply.github.com>
2024-01-13 17:32:56 -0300

    Funcionamento básico do jogo

---
+++ src/scripts/engine.js
@@ -0,0 +1,45 @@
+const emojis = [
+    "🎄",
+    "🎄",
+    "🎃",
+    "🎃",
+    "🎆",
+    "🎆",
+    "🎭",
+    "🎭",
+    "🥚",
+    "🥚",
+    "🔥",
+    "🔥",
+    "💘",
+    "💘",
+    "💀",
+    "💀",
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement
+    ("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length === 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+    
+}
